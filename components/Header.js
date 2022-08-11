ec1e4a99019cd84226ab7ad766e10598845646be
Glenn <glenn.latomme@gmail.com>
2022-08-11 13:59:59 +0200

    making the template work (sort off)

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,80 @@
 import PropTypes from 'prop-types';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faGem from '@fortawesome/fontawesome-free-regular/faGem'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGem } from '@fortawesome/free-regular-svg-icons';
+import React from 'react';
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+class Header extends React.Component {
+  render() {
+    return (
+      <header id="header" style={this.props.timeout ? { display: 'none' } : {}}>
         <div className="logo">
-            {/*<span className="icon fa-diamond"></span>*/}
-            <FontAwesomeIcon icon={faGem} transform="grow-18" />
+          <FontAwesomeIcon icon={faGem}  />
         </div>
         <div className="content">
-            <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
+          <div className="inner">
+            <h1>Dimension</h1>
+            <p>
+              A fully responsive site template designed by{' '}
+              <a href="https://html5up.net">HTML5 UP</a> and released
+              <br />
+              for free under the{' '}
+              <a href="https://html5up.net/license">Creative Commons</a>{' '}
+              license.
+            </p>
+          </div>
         </div>
         <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
+          <ul>
+            <li>
+              <a
+                href="javascript:;"
+                onClick={() => {
+                  this.props.onOpenArticle('intro');
+                }}
+              >
+                Intro
+              </a>
+            </li>
+            <li>
+              <a
+                href="javascript:;"
+                onClick={() => {
+                  this.props.onOpenArticle('work');
+                }}
+              >
+                Work
+              </a>
+            </li>
+            <li>
+              <a
+                href="javascript:;"
+                onClick={() => {
+                  this.props.onOpenArticle('about');
+                }}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="javascript:;"
+                onClick={() => {
+                  this.props.onOpenArticle('contact');
+                }}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
         </nav>
-    </header>
-)
-
-Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+      </header>
+    );
+  }
 }
 
-export default Header
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+};
+
+export default Header;
